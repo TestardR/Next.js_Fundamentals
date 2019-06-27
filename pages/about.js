f9bff126a824c528df45f88e3e239fea182f6187
unknown <rmtestard@gmail.com>
2019-06-27 14:47:22 +0200

    about page added

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const about = () => {
-  return <div>About Page!</div>;
+  return (
+    <section>
+      <h1>About Next JS</h1>
+      <p>
+        Ut nisi incididunt elit Lorem aliqua id fugiat fugiat labore cillum
+        eiusmod amet. Esse duis ipsum ad adipisicing pariatur sunt amet pariatur
+        ipsum adipisicing excepteur. Elit magna laborum minim laborum id id
+        laborum ipsum ad aliquip labore consequat. Duis deserunt veniam aliquip
+        consectetur magna anim. Proident sint voluptate officia dolore duis
+        aliqua enim minim est in reprehenderit aliqua. Consectetur adipisicing
+        dolore ex incididunt do nulla eiusmod ea nisi quis id dolore ipsum.
+        Laborum ut dolore qui dolor ad sunt anim aute dolore.
+      </p>
+    </section>
+  );
 };
 
 export default about;
